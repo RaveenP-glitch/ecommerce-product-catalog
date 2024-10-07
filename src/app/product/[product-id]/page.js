10a124a25bebe00345899111667e47fdd793481e
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2024-10-07 20:34:20 +0530

    Updating the single product page

--- src/app/product/[product-id]/page.js
+++ src/app/product/[product-id]/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SingleProduct = () => {
   return (
     <div className='w-full container mx-auto min-h-screen'>
-        <p className='text-center'>Selected Product</p>
+        <p className='text-center'>Selected Product Page</p>
 
     </div>
   )
